4417c463556f0ad92858d1cff5d1118442dffdee
dugutarithish <146820750+dugutarithish@users.noreply.github.com>
2026-02-04 16:03:30 +0530

    Add example AWS instance resource

--- aws/main.tf
+++ aws/main.tf
@@ -1,4 +1,4 @@
-resource "aws_instance" "this" {
+resource "aws_instance" "example" {
   ami                     = var.ami_value
   instance_type           = var.instance_type_value
   host_resource_group_arn =  var.subnet_id_value
