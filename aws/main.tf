552346026a2a46a5ce83dc41e2ffc628416320da
dugutarithish <146820750+dugutarithish@users.noreply.github.com>
2026-02-04 16:12:24 +0530

    Update main.tf

--- aws/main.tf
+++ aws/main.tf
@@ -1,6 +1,9 @@
 resource "aws_instance" "example" {
   ami                     = var.ami_value
   instance_type           = var.instance_type_value
-  host_resource_group_arn =  var.subnet_id_value
-  tenancy                 = var.host_value
+  subnet_id               = var.subnet_id_value
+ 
+  tags = {
+    Name = "MyTerraformInstance"
+  }
 }
