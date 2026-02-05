786f858c6f936490b96ee41d63bbce4bde91554a
dugutarithish <146820750+dugutarithish@users.noreply.github.com>
2026-02-05 11:28:49 +0530

    Define variables for AMI, instance type, and subnet ID
    Add variables for AMI, EC2 instance type, and subnet ID.

---
+++ module/variable.tf
@@ -0,0 +1,10 @@
+variable "ami_value" {
+  descripation = "THE AMI NUMBER OR"
+}
+
+variable "ec2_instance_type" {
+  descripation "THE EC2 NUMBER OR VALUE"
+}
+
+variable "subnet_id_value" {
+  descriaption "THE SUBNET ID OR VALUR"
