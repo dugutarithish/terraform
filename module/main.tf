02adb64dd4e776cc2a5d6834c4532b791fb04e17
dugutarithish <146820750+dugutarithish@users.noreply.github.com>
2026-02-05 11:52:19 +0530

    Update main.tf

--- module/main.tf
+++ module/main.tf
@@ -1,22 +1,18 @@
 provider "aws" {
-   alias = "ap-south-1"
    region = "ap-south-1"
-
-provider "aws" {
-  alias = "ap-south-1"
-  region = "ap-south-1"
+}
 
 
 module "ec2_instance" {
 sourec = ".module/ec2_instance"
-ami_value = 
-instance)type_value =
-subnet_id_value =
+ami_value           = var.ami_value
+instance_type_value =  var.instance_type_value
+subnet_id_value     =  var.subnet_id_value
 }
 
 module "ec2_instance" {
 sourec = ".module/ec2_instance"
-ami_value = 
-instance)type_value =
-subnet_id_value =
+ami_value           = var.ami_value
+instance)type_value = var.instance_type_value
+subnet_id_value     = var.subnet_id_value
 }
