937479dbf88f81001d7347d55d80acc20eadc074
dugutarithish <146820750+dugutarithish@users.noreply.github.com>
2026-02-05 13:44:54 +0530

    Create variable.tf

---
+++ module/variable.tf
@@ -0,0 +1,16 @@
+variable "ami_value" {
+  Description = "the ami value"
+  type        = string
+}
+
+
+variable "instance_type_value" {
+  Description = "the ami value"
+  type        = string
+}
+
+
+variable "subnet_id_value" {
+  Description = "the ami value"
+  type        = string
+}
