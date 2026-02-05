7aa41d66d1d417bb3429bd691d6b3e22f275025d
dugutarithish <146820750+dugutarithish@users.noreply.github.com>
2026-02-05 11:57:42 +0530

    Fix spelling mistakes in variable descriptions
    Corrected spelling errors in variable descriptions.

--- module/variable.tf
+++ module/variable.tf
@@ -1,14 +1,14 @@
 variable "ami_value" {
-  descripation = "THE AMI NUMBER OR"
+  description = "THE AMI NUMBER OR"
   type         = string
 }
 
 variable "ec2_instance_type" {
-  descripation   = "THE EC2 NUMBER OR VALUE & t2.miceo"
+  description   = "THE EC2 NUMBER OR VALUE & t2.miceo"
   type           = string
 }
 
 variable "subnet_id_value" {
-  descriaption  = "THE SUBNET ID OR VALUR"
+  description  = "THE SUBNET ID OR VALUR"
   type          = string
 }
