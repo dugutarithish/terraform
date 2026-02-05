6552ef7c1c85e394d6b845efe2e4b22ac6c45d3a
dugutarithish <146820750+dugutarithish@users.noreply.github.com>
2026-02-05 23:31:44 +0530

    Update back-end.tf

--- my-deployment/back-end.tf
+++ my-deployment/back-end.tf
@@ -4,6 +4,6 @@ terraform {
     key            = "rithih/terraform.tfstate"
     region         = "ap-south-1"
     encrypt        = true
-    dynamodb_table = "terraform-lock"
+    dynamodb_table = "rithish"
   }
 }
