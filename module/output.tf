3ff315b5bd70dcb53174a131dd9008090e392ed4
dugutarithish <146820750+dugutarithish@users.noreply.github.com>
2026-02-05 11:54:18 +0530

    Change output to use module instance public IPs
    Updated output values to reference module instances.

--- module/output.tf
+++ module/output.tf
@@ -1,4 +1,10 @@
 output "public_ip_address" {
   description = "The public IP address of the EC2 instance"
-  value       = aws_instance.public_ip
+  value       = module.ec2_instance.public_ip
+}
+
+
+output "public_ip_2" {
+  description = "The public IP address of the EC2 instance"
+  value       = module.ec2_instance_2.public_ip
 }
