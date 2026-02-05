4d783b750123f0f0f2fa6a00733fbb9ced646d0c
dugutarithish <146820750+dugutarithish@users.noreply.github.com>
2026-02-05 21:58:08 +0530

    Update main.tf

--- my-deployment/main.tf
+++ my-deployment/main.tf
@@ -6,3 +6,19 @@ module "ec2_cluster" {
   instance_type_value = "t3.micro"
   subnet_id_value     = "subnet-0c8129f74505c641e" # Replace with your actual Subnet ID
 }
+
+
+resource "aws_s3_bucket" "s3_bucket" {
+  bucket = "rithish-terraofrm" # change this
+}
+
+resource "aws_dynamodb_table" "terraform_lock" {
+  name           = "terraform-lock"
+  billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
