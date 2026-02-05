eead88a2d53104c6e3ea9c045a68fe79743444c2
dugutarithish <146820750+dugutarithish@users.noreply.github.com>
2026-02-05 14:00:55 +0530

    Update output.tf

--- module/output.tf
+++ module/output.tf
@@ -1,16 +1,7 @@
-# This goes inside terraform/module/ec2_instance/outputs.tf
-
 output "public_ip" {
-  description = "The public IP of the instance"
-  value       = aws_instance.example.public_ip
+  value = aws_instance.example.public_ip
 }
 
 output "instance_id" {
-  description = "The ID of the instance"
-  value       = aws_instance.example.instance.id
-}
-
-output "private_ip" {
-  description = "The private IP of the instance"
-  value       = aws_instance.example.private_ip
+  value = aws_instance.example.id # Fixed the extra '.instance' in your code
 }
