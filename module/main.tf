346a0f4811986d958c1a973358e87da7c348bd11
dugutarithish <146820750+dugutarithish@users.noreply.github.com>
2026-02-05 12:01:59 +0530

    Update main.tf

--- module/main.tf
+++ module/main.tf
@@ -3,15 +3,15 @@ provider "aws" {
 }
 
 
-module "ec2_instance" {
-sourec = ".module/ec2_instance"
+module "ec2_instance_1" {
+sourec = "./module/ec2_instance"
 ami_value           = var.ami_value
 instance_type_value =  var.instance_type_value
 subnet_id_value     =  var.subnet_id_value
 }
 
-module "ec2_instance" {
-sourec = ".module/ec2_instance"
+module "ec2_instance_2" {
+sourec = "./module/ec2_instance"
 ami_value           = var.ami_value
 instance)type_value = var.instance_type_value
 subnet_id_value     = var.subnet_id_value
