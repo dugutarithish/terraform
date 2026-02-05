8bbdf4c0d446ef7f2fc623cbc5da7e9e38bc2ad1
dugutarithish <146820750+dugutarithish@users.noreply.github.com>
2026-02-05 16:01:04 +0530

    Update main.tf

--- my-deployment/main.tf
+++ my-deployment/main.tf
@@ -2,7 +2,7 @@ module "ec2_cluster" {
   source = "../aws"
 
   # Use the exact variable names your module requires
-  ami_value           = "ami-0c55b159cbfafe1f0" # Replace with a valid AMI ID for your region
+  ami_value           = "ami-019715e0d74f695be" # Replace with a valid AMI ID for your region
   instance_type_value = "t3.micro"
-  subnet_id_value     = "subnet-0123456789abcdef0" # Replace with your actual Subnet ID
+  subnet_id_value     = "subnet-0c8129f74505c641e" # Replace with your actual Subnet ID
 }
