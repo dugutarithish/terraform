1aa4c4fc670e9c58603417da191c75485820c40b
dugutarithish <146820750+dugutarithish@users.noreply.github.com>
2026-02-05 11:44:45 +0530

    Update variable.tf

--- module/variable.tf
+++ module/variable.tf
@@ -1,10 +1,14 @@
 variable "ami_value" {
   descripation = "THE AMI NUMBER OR"
+  type         = string
 }
 
 variable "ec2_instance_type" {
-  descripation "THE EC2 NUMBER OR VALUE"
+  descripation   = "THE EC2 NUMBER OR VALUE & t2.miceo"
+  type           = string
 }
 
 variable "subnet_id_value" {
-  descriaption "THE SUBNET ID OR VALUR"
+  descriaption  = "THE SUBNET ID OR VALUR"
+  type          = string
+}
