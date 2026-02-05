21d43ff7deedc009f6121bdf9c3f6cb695658aa5
dugutarithish <146820750+dugutarithish@users.noreply.github.com>
2026-02-05 22:20:36 +0530

    Delete Day-4 directory

--- Day-4/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "rithish-terraofrm" # change this
-    key            = "rithi/terraform.tfstate"
-    region         = "ap-south-1"
-    encrypt        = true
-    dynamodb_table = "terraform-lock"
-  }
-}
