3cb70f916a1c6d808561ee9fa93913d08411aa61
dugutarithish <146820750+dugutarithish@users.noreply.github.com>
2026-02-05 23:37:19 +0530

    Update back-end.tf

--- my-deployment/back-end.tf
+++ my-deployment/back-end.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "rithissh-s3"
-    key            = "rithih/terraform.tfstate"
+    key            = "rithissh-s3/terraform.tfstate"
     region         = "ap-south-1"
     encrypt        = true
     dynamodb_table = "rithish"
