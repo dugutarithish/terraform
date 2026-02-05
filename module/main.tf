f26aea11e162b8b9abbe16ef63297289cd868559
dugutarithish <146820750+dugutarithish@users.noreply.github.com>
2026-02-05 12:33:19 +0530

    Fix spelling errors in module source definitions

--- module/main.tf
+++ module/main.tf
@@ -1,18 +1,17 @@
 provider "aws" {
-   region = "ap-south-1"
+  region = "ap-south-1"
 }
 
-
 module "ec2_instance_1" {
-   sourec = "./ec2_instance"
-   ami_value           = var.ami_value
-   instance_type_value =  var.instance_type_value
-   subnet_id_value     =  var.subnet_id_value
+  source              = "./ec2_instance"  # Fixed spelling from 'sourec'
+  ami_value           = var.ami_value
+  instance_type_value = var.instance_type_value
+  subnet_id_value     = var.subnet_id_value
 }
 
 module "ec2_instance_2" {
-   sourec = "./ec2_instance"
-   ami_value           = var.ami_value
-   instance)type_value = var.instance_type_value
-   subnet_id_value     = var.subnet_id_value
+  source              = "./ec2_instance"  # Fixed spelling from 'sourec'
+  ami_value           = var.ami_value
+  instance_type_value = var.instance_type_value
+  subnet_id_value     = var.subnet_id_value
 }
